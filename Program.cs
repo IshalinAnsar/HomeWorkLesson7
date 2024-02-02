a32c5584be00997496c6a34e40414c7adab031ec
Ansar Ishalin <elitegentlman.a@gmail.com>
2024-02-02 21:41:05 +0300

    Написал программу версия - 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int m)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
