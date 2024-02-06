cfce7c2ac2503c3fc7c99e51066032b32f74fbc6
Ansar Ishalin <elitegentlman.a@gmail.com>
2024-02-06 19:41:44 +0300

    Были решены все задачи

--- Program.cs
+++ Program.cs
@@ -1,30 +1,44 @@
 ﻿// // Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
-// Console.Write("Введите число M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите число N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-// NaturalToLow(N1, N2);
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int start = m;
+int end = n;
+
+void ShowNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        Console.Write(start);
+        return;
+    }
+    Console.Write(start + " ");
+    ShowNumbers(start + 1, end);
+}
+ShowNumbers(start, end);
+Natural(m,n);
+
+void Natural(int n, int m)
+{
+    if (m > n)
+    {
+        Natural(m, n + 1);
+        Console.Write($" {n}");
+    }
+    else
+    {
+        return;
+    }
+}
 
 
-// void NaturalToLow(int n, int m)
-// {
-//     if (N2 > n)
-//     {
-//         return;
-//     }
-//     else
-//     {
-//         NaturalToLow(n, N1 + 1);
-//         Console.Write(N2 + " ");
-//     }
-// }
 
 
-
-
-// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -57,3 +71,36 @@ int Akkerman(int m, int n)
         return (Akkerman(m - 1, Akkerman(m, n - 1)));
     }
 }
+
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int [] CreateArray (int size, int max, int min)
+{
+    int [] array  = new int[size];
+    Random rand = new();
+        for (int i = 0; i < size; i++)
+        {
+            array [i] = rand.Next(min,max+1);
+        }
+    return array;
+}
+    string PrintArray(int [] array)
+    {
+    return string.Join(", ",array);
+    }
+    int [] ArraySwap(int [] array,int index)
+    {
+
+        if (index <= 0)
+            {
+                return array;
+            }
+        int temp = array[index];
+        array[index] = array[array.Length - index-1];
+        array[array.Length - index-1] = temp;
+        return ArraySwap(array,index -= 2);
+    }
+int [] array = CreateArray(10,10,0);
+    System.Console.WriteLine($"Произвольный массив => [{PrintArray(array)}]");
+    System.Console.WriteLine($"Перевернутый массив => [{PrintArray(ArraySwap(array,array.Length-1))}]");
